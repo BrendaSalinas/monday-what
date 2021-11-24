17c09dda487a9a771ca64590ccdae501e3995a2e
Brenda Salinas <brenda.salinas2128@gmail.com>
2021-11-24 12:55:07 -0600

    add ability to delete a task

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ var taskIdCounter = 0;
 
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var pageContentEl = document.querySelector("#page-content");
 
 var taskFormHandler = function(event) {
   event.preventDefault();
@@ -90,6 +91,24 @@ var createTaskActions = function(taskId) {
   return actionContainerEl;
 };
 
+var taskButtonHandler = function(event) {
+  console.log(event.target);
+
+  if (event.target.matches(".delete-btn")){
+    var taskId = event.target.getAttribute("data-task-id");
+    deleteTask(taskId);
+  }
+};
+
+var deleteTask = function(taskId) {
+  console.log(taskId);
+
+  var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+  taskSelected.remove();
+};
 
 //Create a new task 
 formEl.addEventListener("submit", taskFormHandler);
+
+//for edit and delete buttons 
+pageContentEl.addEventListener("click", taskButtonHandler); 
